1252e571b4f990c6fa63cd0ba0967a7d5fb7f012
jimdaza <63026904+jimdaza@users.noreply.github.com>
2020-05-29 17:45:56 -0500

    Change matrix name from "m" to "data"

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix
+## to avoid the time-costly recalculations
 
-## Write a short comment describing this function
+## This function creates a matrix that "caches" its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmatrix <- NULL
+  set <- function(y){
+    x <<- y
+    invmatrix <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) invmatrix <<- solveMatrix
+  getInverse <- function() invmatrix
+  list(set = set, 
+       get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function shows the inverted matrix created previously and,
+## if the matrix remains unchanged, shows the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invmatrix <- x$getInverse()
+  if (!is.null(invmatrix)) {
+    message("getting cached data")
+    return(invmatrix)
+  }
+  m <- x$get()
+  invmatrix <- solve(m, ...)
+  x$setInverse(invmatrix)
+  invmatrix
 }
